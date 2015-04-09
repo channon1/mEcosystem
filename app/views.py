78e289d7c4e6fd6802d9d4f5da5d655f44a3a8eb
Chris Hannon <channon@hawk.iit.edu>
2015-04-09 09:52:44 -0500

    temp

--- app/views.py
+++ app/views.py
@@ -5,27 +5,25 @@ from flask import render_template,  Response
 import socket
 import time
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html')
-
-@app.route('/data')
-def data():
-    return Response(yunserver_sse(), mimetype='text/event-stream')
-
-
 def yunserver_sse():
     try:
-            soc = socket.create_connection(('localhost', 1993))
+            soc = socket.create_connection(('localhost', 5678))
             socfile = soc.makefile()
             while True:
                 line = socfile.readline()
                 if not line:
                    raise StopIteration
-                yield (line + '\n')
+                yield 'data: {0}\n\n'.format(line)
                 time.sleep(0)
     except socket.error:
             raise StopIteration
 
-            
+@app.route('/data')
+def data():
+    return Response(yunserver_sse(), mimetype='text/event-stream')
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+                
