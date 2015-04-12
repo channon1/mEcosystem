98ae2f52a7330f3290cee038b4f5dc934d327f62
Christopher Hannon <channon@iit.edu>
2015-04-11 21:45:05 -0500

    added 3-Temp, humidity

--- mecosystem/mecosystem.ino
+++ mecosystem/mecosystem.ino
@@ -8,13 +8,13 @@
  */
 
 //type 1 for yun 0 for other
-#define YUN 0
+#define YUN 1
 //temp
 #include <OneWire.h>
 //humidity
 #include <DHT.h>
-//ph?
-#include <SoftwareSerial.h>
+//4char
+//#include <SoftwareSerial.h>
 //yun
 #include <Bridge.h>
 #include <Console.h>
@@ -26,6 +26,16 @@
 #include <YunServer.h>
 
 #define YUN_SERVER_PORT 5678
+
+//ph
+#define phOFF 0.00
+#define ArrayLenth  40    //times of collection
+int pHArray[ArrayLenth];   //Store the average value of the sensor feedback
+int pHArrayIndex=0;
+//what do these do?
+#define samplingInterval 20
+#define printInterval 800
+//end ph
 
 /* Data Pins*/
 #define lightT 3
@@ -58,6 +68,7 @@ DHT dht(DHTPIN, DHTTYPE);
 
 //TODO: flippin organize this better
 
+//who made these names???? satan, I hope i didnt
 //global variables
 //data variables
 //min on
@@ -495,8 +506,27 @@ float getTempFish() {
 // Thank satan thats over...
 ////////////////////////////////////// END TEMP //////////////////////////////
 
-void getPH() {
-
+float getPH() {
+  static unsigned long samplingTime = millis();
+  static unsigned long printTime = millis();
+  static float pHValue,voltage;
+  if(millis()-samplingTime > samplingInterval)
+  {
+    pHArray[pHArrayIndex++]=analogRead(ph);
+    if(pHArrayIndex==ArrayLenth)pHArrayIndex=0;
+    voltage = avergearray(pHArray, ArrayLenth)*5.0/1024;
+    pHValue = 3.5*voltage+phOFF;
+    samplingTime=millis();
+  }
+  if(millis() - printTime > printInterval)   //Every 800 milliseconds, print a numerical, convert the state of the LED indicator
+  {
+    Serial.print("Voltage:");
+    Serial.print(voltage,2);
+    Serial.print("    pH value: ");
+    Serial.println(pHValue,2);
+    printTime=millis();
+    return pHValue;
+  }
 }
 
 //////////////////////////////////// HUMIDITY ///////////////////////////////
@@ -535,6 +565,52 @@ float getHumidityPlants() {
 ///////////////////// END HUMIDITY ////////////////////////////
 
 //TODO: WE need a mushroom Humidity 
+
+// ph helper .. ikr 
+double avergearray(int* arr, int number){
+  int i;
+  int max,min;
+  double avg;
+  long amount=0;
+  if(number<=0){
+    Serial.println("Error number for the array to avraging!/n"); //wow who wrote this?
+    return 0;
+  }
+  if(number<5){   //less than 5, calculated directly statistics
+    for(i=0;i<number;i++){
+      amount+=arr[i];
+    }
+    avg = amount/number;
+    return avg;
+  }
+  else{
+    if(arr[0]<arr[1]){
+      min = arr[0];
+      max=arr[1];
+    }
+    else{
+      min=arr[1];
+      max=arr[0];
+    }
+    for(i=2;i<number;i++){
+      if(arr[i]<min){
+        amount+=min;        //arr<min
+        min=arr[i];
+      }
+      else {
+        if(arr[i]>max){
+          amount+=max;    //arr>max
+          max=arr[i];
+        }
+        else{
+          amount+=arr[i]; //min<=arr<=max
+        }
+      }//if
+    }//for
+    avg = (double)amount/(number-2);
+  }//if
+  return avg;
+}
 
 ////////////////////////  END HELPERs  ///////////////////////////////
 
